4bb39f0b02453be07e15d93d51d7a288bab219f9
Your Name <N/A>
2025-11-22 09:53:59 +0000

    rounding errors

--- frontEnd/mapInteractions.py
+++ frontEnd/mapInteractions.py
@@ -66,7 +66,7 @@ def set_total_distance(total_distance,age_group,gender):
         if str(distanceElevation[1]) =='None' or str(distanceElevation[1]) == 0:
             elevation = 0
         else:
-            elevation = (round(int(distanceElevation[1])), 2)
+            elevation = round(int(distanceElevation[1]), 2)
         st.session_state["elevation"] = str(elevation) + ' m'
         predictedTime = machineLearningUtils(distanceElevation[0],age_group,gender_label).predictModel()
         st.session_state["predicted_time_input"] = str(round(int(predictedTime[0]), 2)) + ' minutes'
